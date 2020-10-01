3843d35a742f9d1d7162f1b6e8b87156f431aea8
Trung Le D <trung.le@asiantech.vn>
2020-10-01 17:22:38 +0700

    Detail VC

--- FinalProject/Library/Ext/UIVIewControllerExt.swift
+++ FinalProject/Library/Ext/UIVIewControllerExt.swift
@@ -11,9 +11,10 @@ import UIKit
 extension UIViewController {
     
     func showAlert(alertText : String, alertMessage : String) {
-        let alert = UIAlertController(title: alertText, message: alertMessage, preferredStyle: UIAlertController.Style.alert)
+        let alert = UIAlertController(title: alertText,
+                                      message: alertMessage,
+                                      preferredStyle: UIAlertController.Style.alert)
         alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-    self.present(alert, animated: true, completion: nil)
-      }
-
+        self.present(alert, animated: true, completion: nil)
+    }
 }
