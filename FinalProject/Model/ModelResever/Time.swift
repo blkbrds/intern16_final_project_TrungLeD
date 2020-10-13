5ce94e3c5fefce0de4795448be69c1c757fdcb2c
Trung Le D <trung.le@asiantech.vn>
2020-10-13 11:45:38 +0700

    fix convention

--- FinalProject/Model/ModelResever/Time.swift
+++ FinalProject/Model/ModelResever/Time.swift
@@ -2,24 +2,22 @@ import Foundation
 import ObjectMapper
 
 final class Time: Mappable {
-	var id: Int = 0
-	var startTime: String = ""
-	var endTime: String = ""
-
+    var id: Int = 0
+    var startTime: String = ""
+    var endTime: String = ""
+    
     init?(map: Map) {
-	}
+    }
     
     init(id: Int = 0, startTime: String = "", endTime: String = "") {
         self.id = id
         self.startTime = startTime
         self.endTime = endTime
     }
-
+    
     func mapping(map: Map) {
-
-		id <- map["id"]
-		startTime <- map["start_time"]
-		endTime <- map["end_time"]
-	}
-
+        id <- map["id"]
+        startTime <- map["start_time"]
+        endTime <- map["end_time"]
+    }
 }
